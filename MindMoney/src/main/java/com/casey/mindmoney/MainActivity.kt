02cc189716653b5ca6b2a6eeb87aaaa8b9ca1fb3
Casey Summers <casey.summers@my.jcu.edu.au>
2025-06-07 00:22:16 +1000

    Fixes structure and MainActivity bug

--- MindMoney/src/main/java/com/casey/mindmoney/MainActivity.kt
+++ MindMoney/src/main/java/com/casey/mindmoney/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.casey.mindmoney.screens.HomePageScreen
 import com.casey.mindmoney.ui.theme.AppTheme
 
@@ -29,13 +27,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Preview(name = "Preview Mode", showBackground = true, showSystemUi = true)
-@Composable
-fun HomePageLightPreview() {
-    AppTheme(darkTheme = false, dynamicColor = false) {
-        HomePageScreen()
     }
 }
