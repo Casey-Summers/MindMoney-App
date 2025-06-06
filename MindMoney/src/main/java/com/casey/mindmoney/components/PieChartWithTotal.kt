57dab8f48d518d07de7ae40b3805a0eb1a2fe8c1
Casey Summers <casey.summers@my.jcu.edu.au>
2025-06-07 00:03:02 +1000

    Optimises imports and references

--- MindMoney/src/main/java/com/casey/mindmoney/components/PieChartWithTotal.kt
+++ MindMoney/src/main/java/com/casey/mindmoney/components/PieChartWithTotal.kt
@@ -1,7 +1,10 @@
 package com.casey.mindmoney.components
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
